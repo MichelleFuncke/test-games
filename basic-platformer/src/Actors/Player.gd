6d2b0ca7809539eef1122ddc30911e070752af66
Michelle A Funcke <kogelmichelle@gmail.com>
2020-05-17 14:40:47 -0400

    fix: rename
    Using the naming convensions

--- basic-platformer/src/Actors/Player.gd
+++ basic-platformer/src/Actors/Player.gd
@@ -2,10 +2,10 @@ extends Actor
 
 
 func _physics_process(delta: float) -> void:
-	var is_jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
-	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL) # we don't need to multiply by delta because move_and_slide
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL) # we don't need to multiply by delta because move_and_slide
 
 
 func get_direction() -> Vector2:
@@ -20,11 +20,11 @@ func calculate_move_velocity(
 		speed: Vector2,
 		is_jump_interrupted: bool
 	) -> Vector2:
-	var new_velocity: = linear_velocity
-	new_velocity.x = speed.x * direction.x
-	new_velocity.y += gravity * get_physics_process_delta_time() # this is the delta _process uses	
+	var out: = linear_velocity
+	out.x = speed.x * direction.x
+	out.y += gravity * get_physics_process_delta_time() # this is the delta _process uses	
 	if direction.y == -1.0:
-		new_velocity.y = speed.y * direction.y
+		out.y = speed.y * direction.y
 	if is_jump_interrupted: # Jump is now modulated by how long you press jump
-		new_velocity.y = 0.0
-	return new_velocity
+		out.y = 0.0
+	return out
