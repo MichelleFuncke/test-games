aa0740062166318e57d66934f7ba1e2a2c55f0ce
Michelle A Funcke <kogelmichelle@gmail.com>
2020-05-17 17:29:16 -0400

    Enemies can die but the player can't which doesn't make sense

--- basic-platformer/src/Actors/Enemy.gd
+++ basic-platformer/src/Actors/Enemy.gd
@@ -5,6 +5,14 @@ func _ready() -> void:
 	set_physics_process(false) # This will make sure the enemy only starts moving once it becomes part of the view
 	_velocity.x = -speed.x
 
+
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
+
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	if is_on_wall():
--- basic-platformer/src/Actors/Player.gd
+++ basic-platformer/src/Actors/Player.gd
@@ -1,6 +1,14 @@
 extends Actor
 
 
+export var stomp_impulse: = 1000.0
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+	
+func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
+	queue_free()
+
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
@@ -28,3 +36,15 @@ func calculate_move_velocity(
 	if is_jump_interrupted: # Jump is now modulated by how long you press jump
 		out.y = 0.0
 	return out
+
+
+func calculate_stomp_velocity(
+		linear_velocity: Vector2,
+		impulse: float
+	) -> Vector2:
+	var out: = linear_velocity
+	out.y = -impulse
+	return out
+
+
+
