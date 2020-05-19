0f1184629dba26e977f91fd497ed8a70caf22056
Michelle A Funcke <kogelmichelle@gmail.com>
2020-05-19 17:05:14 -0400

    feat: hooked PlayerData to the userinterface
    hooked PlayerData to the userinterface
    Also added the user interface to all the levels

--- basic-platformer/src/UserInterface/UserInterface.gd
+++ basic-platformer/src/UserInterface/UserInterface.gd
@@ -3,13 +3,27 @@ extends Control
 
 onready var scene_tree: = get_tree()
 onready var pause_overlay: ColorRect = get_node("PauseOverlay")
+onready var score: Label = get_node("Label")
+onready var pause_title: Label = get_node("PauseOverlay/Title")
 
 var paused: = false setget set_paused
+
+func _ready() -> void:
+	PlayerData.connect("score_updated", self, "update_interface")
+	PlayerData.connect("player_died", self, "_on_PlayerData_player_died")
+	update_interface()
+
+func _on_PlayerData_player_died() -> void:
+	self.paused = true
+	pause_title.text = "You died"
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		self.paused = not paused # Goes through the setter function
 		scene_tree.set_input_as_handled() # Stops the "pause" event from triggering other areas of the code
+
+func update_interface() -> void:
+	score.text = "Score: %s" % PlayerData.score
 
 func set_paused(value: bool) -> void:
 	paused = value
