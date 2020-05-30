1294448bdad8af7f12f9c1a44d9d981d5e66f5d5
Michelle A Funcke <kogelmichelle@gmail.com>
2020-05-30 16:46:03 -0400

    feat: Enemies don't walk off ledges

--- basic_my_game/src/actors/Enemy_elf.gd
+++ basic_my_game/src/actors/Enemy_elf.gd
@@ -3,14 +3,20 @@ extends Actor
 export var score: = 100
 
 func _ready() -> void:
-	# set_physics_process(false)
-	_velocity.x = -speed.x
+	set_physics_process(false)
+	#_velocity.x = -speed.x
 
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	if is_on_wall():
-		_velocity *= -1.0
+		_velocity.x *= -1.0
+	
+	if $RayCast2D.is_colliding() == false:
+		_velocity.x *= -1.0
+		
 	animate_sprite(_velocity)
+	ledge_detection(_velocity)
+	
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
 
 
@@ -20,3 +26,12 @@ func animate_sprite(direction: Vector2) -> void:
 	else:
 		$AnimatedSprite.play("walk")
 		$AnimatedSprite.flip_h = direction.x < 0
+		
+func ledge_detection(direction: Vector2) -> void:
+	if sign($RayCast2D.position.x) != sign(direction.x):
+		$RayCast2D.position.x *= -1.0
+
+
+func _on_screen_entered() -> void:
+	set_physics_process(true)
+	_velocity.x = -speed.x
