83b5b75a0d7694fd50beba3ae9d6d220c7f114ab
Michelle A Funcke <kogelmichelle@gmail.com>
2020-05-29 20:23:46 -0400

    feat: damage
    I have tried to add a damaged animation
    The animation should work but I don't have any enemies to test it on yet

--- basic_my_game/src/actors/Actor.gd
+++ basic_my_game/src/actors/Actor.gd
@@ -7,3 +7,15 @@ const FLOOR_NORMAL: = Vector2.UP
 export var speed: = Vector2(100.0, 500.0)
 export var gravity: = 3000.0
 var _velocity: = Vector2.ZERO
+export var max_health: = 3000
+var _health : = max_health
+
+func take_damage(damage: int) -> void:
+	_health -= damage
+	if _health <= 0:
+		die()
+
+func die() -> void:
+	# Might want to play death animation or count the dealths
+	get_node("AnimationPlayer").play("damaged")
+	queue_free()
