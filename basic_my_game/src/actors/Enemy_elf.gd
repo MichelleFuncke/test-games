6181b0a6a950582128ef91d648ebf2a887112fa5
Michelle A Funcke <kogelmichelle@gmail.com>
2020-05-30 18:56:57 -0400

    fix: set stage and camera
    Set the correct stage size so the game isn't for ants
    Fixed the enemies to only start moving once they enter the screen
    Added a camera to the player

--- basic_my_game/src/actors/Enemy_elf.gd
+++ basic_my_game/src/actors/Enemy_elf.gd
@@ -3,10 +3,8 @@ extends Actor
 export var score: = 100
 
 func _ready() -> void:
-	set_physics_process(false)
 	# Have to set it here or it uses the default of the Actor.gd
 	_health = max_health
-	#_velocity.x = -speed.x
 
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
@@ -35,7 +33,6 @@ func ledge_detection(direction: Vector2) -> void:
 
 
 func _on_screen_entered() -> void:
-	set_physics_process(true)
 	_velocity.x = -speed.x
 
 
