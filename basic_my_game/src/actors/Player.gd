165f7dc5252fe90a66e16873ce4bbec9997cd285
Michelle A Funcke <kogelmichelle@gmail.com>
2020-06-07 18:06:04 -0400

    fix: separate jump and up
    Jump is now only triggered by the spacebar
    Up is triggered by w and arrow up but only works on ladders

--- basic_my_game/src/actors/Player.gd
+++ basic_my_game/src/actors/Player.gd
@@ -41,7 +41,7 @@ func get_direction() -> Vector2:
 	var side_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var vertical_movement = 1.0
 	if is_on_ladder:
-		vertical_movement = Input.get_action_strength("move_down") - Input.get_action_strength("jump")
+		vertical_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	else:
 		vertical_movement = -1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	return Vector2(side_movement, vertical_movement)
