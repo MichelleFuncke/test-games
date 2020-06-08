6bfe5c7a2dded27eb33f80bb0bde29051ad1f6af
Michelle A Funcke <kogelmichelle@gmail.com>
2020-06-07 21:22:51 -0400

    feat: Double jump
    Reduced gravity
    Reduced jump speed
    Implemented double jump
    NOTE: Player can't jump off ladders

--- basic_my_game/src/actors/Player.gd
+++ basic_my_game/src/actors/Player.gd
@@ -4,18 +4,26 @@ const FIREBALL = preload("res://src/objects/Fireball.tscn")
 signal direction_changed
 var is_on_ladder: = false
 var ladder_count: = 0
+var was_on_ground: = true
+var jump_count: = 0
 
 
 func _ready() -> void:
 	# Have to set it here or it uses the default of the Actor.gd
 	self.connect("direction_changed", self, "change_direction")
 	PlayerData.connect("on_ladder", self, "_on_ladder_changed")
+	jump_count = 0
 
 # warning-ignore:unused_argument
 func _physics_process(delta: float) -> void:
 	if _is_dead == true || _is_damaged == true:
 		return
-		
+	
+	var can_jump: = false
+	if Input.is_action_just_pressed("jump") and jump_count < 2:
+		jump_count += 1
+		can_jump = true
+	
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	is_on_ladder = ladder_count > 0
 	
@@ -24,11 +32,15 @@ func _physics_process(delta: float) -> void:
 			item.get_node("PassThroughTimer").start()
 			item.set_collision_layer_bit(3, false)
 	
-	var direction: = get_direction()
+	var direction: = get_direction(can_jump)
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	animate_sprite(_velocity)
 	
 	_velocity = move_and_slide(_velocity, WorldData.FLOOR_NORMAL) # we don't need to multiply by delta because move_and_slide
+	
+	was_on_ground = is_on_floor() || is_on_ladder
+	if was_on_ground:
+		jump_count = 0
 	
 	if Input.is_action_just_pressed("ui_focus_next") and not is_on_ladder:
 		var fireball = FIREBALL.instance()
@@ -37,13 +49,13 @@ func _physics_process(delta: float) -> void:
 		fireball.position = $Position2D.global_position
 
 
-func get_direction() -> Vector2:
+func get_direction(can_jump: bool) -> Vector2:
 	var side_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var vertical_movement = 1.0
 	if is_on_ladder:
 		vertical_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	else:
-		vertical_movement = -1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+		vertical_movement = -1.0 if can_jump else 1.0
 	return Vector2(side_movement, vertical_movement)
 
 
@@ -59,10 +71,10 @@ func calculate_move_velocity(
 	if is_on_ladder:
 		out.y = climb_speed * direction.y
 	else:
-		out.y += WorldData.GRAVITY * get_physics_process_delta_time() # this is the delta _process uses	
-	
-		if direction.y == -1.0:
+		if direction.y < 0:
 			out.y = speed.y * direction.y
+			
+		out.y += WorldData.GRAVITY * get_physics_process_delta_time() # this is the delta _process uses
 	
 	if is_jump_interrupted: # Jump is now modulated by how long you press jump
 		out.y = 0.0
