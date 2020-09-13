66328515e9664a3f6e482cab633ca458e6857822
Michelle <kogelmichelle@gmail.com>
2020-09-13 11:16:20 -0400

    fix: commit missing assets
    All images were missing from the repo

--- basic_my_game/src/autoload/ImportData.gd
+++ basic_my_game/src/autoload/ImportData.gd
@@ -5,13 +5,13 @@ var loot_data
 
 func _ready() -> void:
 	var itemdata_file = File.new()
-	itemdata_file.file.open("res://Data/ItemTable.json", File.READ)
+	itemdata_file.open("res://data/ItemTable.json", File.READ)
 	var itemdata_json = JSON.parse(itemdata_file.get_as_text())
 	itemdata_file.close()
 	item_data = itemdata_json.result
 	
 	var lootdata_file = File.new()
-	lootdata_file.file.open("res://Data/LootTable.json", File.READ)
+	lootdata_file.open("res://data/LootTable.json", File.READ)
 	var lootdata_json = JSON.parse(lootdata_file.get_as_text())
 	lootdata_file.close()
 	loot_data = lootdata_json.result
